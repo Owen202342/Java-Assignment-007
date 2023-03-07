64c2fc94c5a8b109699fd7c98a369f1cf3d2ae8c
owenv <owenvanmantgem@gmail.com>
2023-03-06 21:01:20 -0600

    All done

--- src/MyMath.java
+++ src/MyMath.java
@@ -2,6 +2,28 @@ import java.util.Scanner;
 
 public class MyMath {
 
+    public static int gcf(int a, int b) {
+
+        int c = 0;
+
+        while(b != 0) {
+            if (a > b) {
+                c = b;
+                b = a % b;
+                a = c;
+            }
+            else if (!(a >b)) {
+                b = b%a;
+            }
+
+
+
+        }
+        return(a);
+
+    }
     public static void main(String[] args) {
+
+        System.out.println(gcf(42, 21));
     }
 }
